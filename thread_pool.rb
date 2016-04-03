dab2bb31ac102728155d287d4914e6360b6f542d
Alexandru Calinoiu <calinoiu.alexandru@agilefreaks.com>
2016-04-03 19:56:54 +0300

    add failing thread pool initial test

---
+++ thread_pool.rb
@@ -0,0 +1,14 @@
+# pool = ThreadPool.new(size: 5)
+# pool.schedule { do_work }
+# pool.shutdown
+
+class ThreadPool
+	def initialize(size:)
+	end
+
+	def schedule(&black)
+	end
+	
+	def shutdown
+	end
+end
